62581f44ee0ae5692539a51f79f4f28419d982b1
Ezeora Kenechukwu Johnbosco <106724858+Ezeora-Kenechukwu@users.noreply.github.com>
2025-01-17 17:52:43 +0100

    First Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ console.log('====================================');
+ console.log('hello');
+ console.log('====================================');
+
+  return (
+    <>
+     <div className="">
+      <h1 className="text-purple-300 text-center font-black">Hello World</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
