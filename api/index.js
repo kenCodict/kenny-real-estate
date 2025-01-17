242dc5f1140749ff7bcbfb166fa2c07841a8b8fe
Ezeora Kenechukwu Johnbosco <106724858+Ezeora-Kenechukwu@users.noreply.github.com>
2025-01-17 21:33:18 +0100

    Setting up the database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,23 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
+const uri = process.env.MONGOURL;
 
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+async function run() {
+    try {
+      // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+      await mongoose.connect(uri, clientOptions);
+      await mongoose.connection.db.admin().command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await mongoose.disconnect();
+    }
+  }
+  run().catch(console.dir);
 const app = express();
 app.listen(3000, () => {
     console.log('====================================');
