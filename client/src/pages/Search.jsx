02317c0f4d1d3a1972eca925ae9bfb5af7c87400
Ezeora Kenechukwu Johnbosco <106724858+Ezeora-Kenechukwu@users.noreply.github.com>
2025-02-02 12:48:42 +0100

    add on search functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,29 +1,107 @@
-import { useState } from "react";
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from 'react-router-dom'
 const Search = () => {
 const [data, setData] = useState({
     type:'all',
   searchTerm:'',
   offer:true,
-  parking:'',
-  furnished:'',
+  parking:false,
+  furnished:false,
+  sort:"createdAt",
+  order:'desc'
 })
-
+const navigate = useNavigate()
     const [error, setError] = useState("");
+    const [listings, setListing] = useState("");
     const [success, setSuccess] = useState("");
     const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
         const { name, type, checked, value } = e.target;
-        setData({
-          ...data,
-          [name]: type === 'checkbox' ? checked : value,
-        });
+        if (name == 'sort_order') {
+            const sort = value.split('_')[0] || 'createdAt';
+            const order = value.split('_')[1] || 'desc';
+            setData({...data, order:order, sort:sort})
+        }else {
+   setData({
+     ...data,
+     [name]: type === "checkbox" ? checked : value,
+   });
+        }
+     
       };
+const search = async (e) => {
+e.preventDefault()
+const urlParams = new URLSearchParams(window.location.search);
+urlParams.set("searchTerm", data.searchTerm);
+urlParams.set("offer", data.offer);
+urlParams.set("sort", data.sort);
+urlParams.set("type", data.type);
+urlParams.set("furnished", data.furnished);
+urlParams.set("parking", data.parking);
+const searchQuery = urlParams.toString()
+navigate(`/search?${searchQuery}`)
+}
+useEffect(() => {
+  const urlParams = new URLSearchParams(location.search);
+  const searching = urlParams.get("searchTerm");
+  const offer = urlParams.get("offer");
+  const sort = urlParams.get("sort");
+  const order = urlParams.get("order");
+  const type = urlParams.get("type");
+  const furnished = urlParams.get("furnished");
+  const parking = urlParams.get("parking");
+
+  if (searching ||offer ||sort ||type || furnished||parking ) {
+    setData({
+      ...data,
+      searchTerm: searching || "",
+      offer: offer == "true" ? true : false,
+      sort: sort || 'createdAt',
+      order: order || 'desc',
+      type: type || "all",
+      furnished: furnished == "true" ? true : false,
+      parking: parking == "true" ? true : false,
+    });
+  }
+  const searchQuery = urlParams.toString();
+  fetchListings(searchQuery);
+},[])
+
+const fetchListings = async (searchQuery) => {
+  setLoading(true);
+  try {
+    const res = await axios.get(`/api/listing?${searchQuery}`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const dat = res.data;
+
+    if (dat.success === false) {
+      setError(dat.message);
+      // setError(dat.message);
+      setLoading(false);
+    } else {
+      setSuccess(dat.message);
+      setListing(dat.data);
+      console.log("====================================");
+      console.log(dat.data);
+      console.log("====================================");
+
+      setLoading(false);
+    }
+  } catch (error) {
+    console.log(error);
+    setError(error.response?.data?.message || "Something went wrong");
+    setLoading(false);
+  }
+};
   return (
-    <main className="flex flex-wrap gap-5 ">
-      <section className="border-b border-b-gray-400 md:border-b-0 md:border-r md:border-r-gray-400 p-4 min-w-[200px]">
-        <form action="" className="">
+    <main className="flex flex-wrap gap-5 min-h-screen ">
+      <section className="border-b border-b-gray-200 md:border-b-0 md:border-r  md:border-r-gray-200 p-4 min-w-[200px] ">
+        <form action="" className="" onSubmit={search}>
           <div className="flex gap-4 items-center">
             <label htmlFor="searchTerm" className="font-black">
               Search Term:
@@ -121,15 +199,17 @@ const [data, setData] = useState({
                 Sort:{" "}
               </label>
               <select
-                name="sort"
-                id="sort"
+                name="sort_order"
+                id="sort_order"
                 className="w-[150px] py-2 px-5 rounded-lg"
                 onChange={handleChange}
+                // defaultValue={`${data.sort}_${data.order}`}
+                value={`${data.sort}_${data.order}`}
               >
-                <option value="latest">Price high to low</option>
-                <option value="latest">Price low to high</option>
-                <option value="latest">Oldest</option>
-                <option value="latest">Latest</option>
+                <option value="regularPrice_desc">Price high to low</option>
+                <option value="regularPrice_asc">Price low to high</option>
+                <option value="createAt_asc">Oldest</option>
+                <option value="createAt_desc">Latest</option>
               </select>
             </div>
           </article>
@@ -148,6 +228,8 @@ const [data, setData] = useState({
         <h1 className="font-black text-3xl p-3 border-b text-slate-700">
           Listing Results:
         </h1>
+
+        
       </section>
     </main>
   );
